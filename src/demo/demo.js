a7c543616c6cd81e20ae0706fdfef8aa068c1f1b
guoqing.dong <878744510@qq.com>
2021-02-02 17:22:10 +0800

    cc

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -1,6 +1,6 @@
 //防抖函数
 
-const oInput = document.getElementById('inp')
+const oInput = document.getElementById("inp");
 // const debounce = (fn) => {
 //     let timer = null
 //     return (...arg) => {
@@ -22,7 +22,7 @@ const oInput = document.getElementById('inp')
 //             isRun = true
 //         },4000)
 
-//     } 
+//     }
 // }
 // const debounce = (fn, delay = 4000) => {
 //     let timer = null;
@@ -34,100 +34,95 @@ const oInput = document.getElementById('inp')
 //     }
 // }
 
-const debounce = (fn,delay = 1000) => {
-    let timer = null;
-    return (...args) => {
-        clearTimeout(timer)
-        timer = setTimeout(() => {
-           fn.call(this, ...args) 
-        }, delay);
-    }
-}
+const debounce = (fn, delay = 1000) => {
+	let timer = null;
+	return (...args) => {
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			fn.call(this, ...args);
+		}, delay);
+	};
+};
 
 const throttle = (fn, delay = 4000) => {
-    let isRun = true;
-    return (...arg) => {
-        if (!isRun) return;
-        isRun = false;
-        setTimeout(() => {
-            fn.call(this, ...arg);
-            isRun = true
-        },delay)
-    }
+	let isRun = true;
+	return (...arg) => {
+		if (!isRun) return;
+		isRun = false;
+		setTimeout(() => {
+			fn.call(this, ...arg);
+			isRun = true;
+		}, delay);
+	};
+};
+
+oInput.addEventListener(
+	"input",
+	throttle((e) => {
+		setTimeout(() => {
+			console.log(222, e);
+		});
+	})
+);
+
+//new
+function _new() {
+	let [fn, ...arg] = [...arguments];
+	let obj = Object.create(fn.prototype);
+	let rt = fn.apply(obj, arg);
+	return typeof rt === "object" ? rt : obj;
 }
-
-
-oInput.addEventListener('input', throttle((e) => {
-    setTimeout(() => {
-        console.log(222, e)
-    })
-}))
-
-
-//new 
-function _new(){
-    let [fn,...arg] = [...arguments];
-    let obj = Object.create(fn.prototype);
-    let rt = fn.apply(obj,arg);
-    return  typeof rt === 'object' ? rt : obj
-
+function Person(name) {
+	this.name = name;
 }
-function Person(name){
-    this.name = name  
-}
-let p = _new(Person,'j')
-let p2 = _new(Person,'jccc')
-console.log(p.__proto__ === Person.prototype)
-console.log(p)
-console.log(p2)
+let p = _new(Person, "j");
+let p2 = _new(Person, "jccc");
+console.log(p.__proto__ === Person.prototype);
+console.log(p);
+console.log(p2);
 let my = {
-    value:'1212',
-}
+	value: "1212",
+};
 //call
 
-Function.prototype.myCall = function(context){
-    const ctx = context || window;
-    ctx.fn = this;
-    let [m,...rest] = [...arguments];
-    let res = ctx.fn(...rest);
-    delete ctx.fn
-    console.log(res)
-    return res 
+Function.prototype.myCall = function (context) {
+	const ctx = context || window;
+	ctx.fn = this;
+	let [m, ...rest] = [...arguments];
+	let res = ctx.fn(...rest);
+	delete ctx.fn;
+	console.log(res);
+	return res;
+};
+function mc(...rest) {
+	console.log(this);
+	console.log(this, rest);
 }
-function mc(...rest){
-    console.log(this)
-    console.log(this,rest)
-}
-// mc.myCall(my,1,2,3,4)
+mc.myCall(my, 1, 2, 3, 4);
 
-
-Function.prototype.myApply = function(context){
-    const ctx = context || window;
-    ctx.fn = this;
-    let res = ctx.fn(...arguments[1])
-    delete ctx.fn;
-    return res
-
-}
+Function.prototype.myApply = function (context) {
+	const ctx = context || window;
+	ctx.fn = this;
+	let res = ctx.fn(...arguments[1]);
+	delete ctx.fn;
+	return res;
+};
 // mc.myApply(my,[1,2,6,7])
 
-
-Function.prototype.myBind = function(context){
-    const ctx = context || window;
-    ctx.fn = this;
-    const args = Array.from(arguments).slice(1);
-    return function(...rest) {
-        let all = [...args,...rest];
-        return ctx.fn(...all)
-    }
-
-}
+Function.prototype.myBind = function (context) {
+	const ctx = context || window;
+	ctx.fn = this;
+	const args = Array.from(arguments).slice(1);
+	return function (...rest) {
+		let all = [...args, ...rest];
+		return ctx.fn(...all);
+	};
+};
 let a = {
-    cc:'111'
-}
-let bindMc = mc.myBind(my)
-bindMc.myCall(a)
-bindMc(1,2,3)
-
+	cc: "111",
+};
+let bindMc = mc.myBind(my);
+bindMc.myCall(a);
+bindMc(1, 2, 3);
 
 ///asdfsd
--- src/index.js
+++ src/index.js
@@ -1,28 +1,46 @@
-import {
-    GenericUtils
-} from '../lib/index'
-const {
-    deepClone
-} = GenericUtils
-
+import { GenericUtils } from "../lib/index";
+const { deepClone } = GenericUtils;
 
 let obj = {
-    a: 1,
-    b: {
-        bb: 'bb',
-        fn: function () {
-            console.log(this.bb)
-        },
-        arr: [{
-            arr1: 'arr1',
-            arr2: 'arr2'
-        }, {
-            arr1: 'arr11',
-            arr2: 'arr22'
-        }, ]
-    },
-    cc: [123, 23, 1233, ]
+	a: 1,
+	b: {
+		bb: "bb",
+		fn: function () {
+			console.log(this.bb);
+		},
+		arr: [
+			{
+				arr1: "arr1",
+				arr2: "arr2",
+			},
+			{
+				arr1: "arr11",
+				arr2: "arr22",
+			},
+		],
+	},
+	cc: [123, 23, 1233],
+};
+let deepObj = deepClone(obj, true);
+console.log(obj.b.fn === deepObj.b.fn);
+console.log(obj.b.arr[1] === deepObj.b.arr[1]);
+
+function isToday(str) {
+	var d = new Date(str);
+	var todaysDate = new Date();
+	if (d.setHours(0, 0, 0, 0) == todaysDate.setHours(0, 0, 0, 0)) {
+		return true;
+	} else {
+		return false;
+	}
 }
-let deepObj = deepClone(obj,true)
-console.log(obj.b.fn === deepObj.b.fn)
-console.log(obj.b.arr[1] === deepObj.b.arr[1])
+// console.log("<<<<<<", isToday(new Date().getTime())); //返回true
+
+function isSameWeek(old, now) {
+	var oneDayTime = 1000 * 60 * 60 * 24;
+	var old_count = parseInt(old.getTime() / oneDayTime);
+	var now_other = parseInt(now.getTime() / oneDayTime);
+	return parseInt((old_count + 4) / 7) == parseInt((now_other + 4) / 7);
+}
+
+console.log("??", isSameWeek(new Date("2020-12-26 23:59:59"), new Date()));
